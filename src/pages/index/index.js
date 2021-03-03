208fd2195518d4869d0c00f0713eff2f0049370e
shifeng <825029499@qq.com>
2021-03-03 13:09:12 +0800

    [feat]样式调整2

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -103,4 +103,49 @@ $(document).ready(() => {
       isFullScreen: window.innerWidth <= 500,
     });
   });
+  // 课堂文案切换
+  $(".class-section .class-wrap").hover((event) => {
+    let $el = $(event.target);
+    if (!$el.hasClass("class-wrap")) {
+      $el = $el.parent(".class-wrap");
+    }
+    if (!$el.data("index") && $el.data("index") !== 0) {
+      return;
+    }
+    const descriptionMap = [
+      {
+        title: "汤林鹏：秒级 20 亿，海量高速高精度海量图像比对",
+        desc:
+          "利用多尺度的图像表示、高速异构的比对框架和分布式的并行处理，实现了 20 亿指纹秒级精准搜索的现实需求",
+      },
+      {
+        title: "张青笛：异构并行计算和性能优化",
+        desc:
+          "基于 CUDA 编程框架的并行计算算法，通过对内存访问、局部存储、代码分支、指令优化、数据传输等一系列的优化，实现了比对性能质的飞跃",
+      },
+      {
+        title: "刘勤：持久内存在图像搜索系统中的应用",
+        desc:
+          " 通过最新的非易失性内存（NVDIMM）技术和 Linux 系统中的内存映射技（mmap）将异构并行计算算法需要的所有图片特征存储在一个分布式的内存数据库内，完成大大降低了海量图像搜索成本，并增强了分布式图像搜索系统的稳定",
+      },
+      {
+        title: "非接触 3D+AI 警用指纹采集仪",
+        desc:
+          "采集技术和指纹采集仪结合，实现伸手即采。一次伸手，同时完成平面和滚动捺印的采集，避免了传统指纹采集设备在手指按压、滚动中造成的形变和模糊",
+      },
+    ];
+    $(".class-section .description").text(
+      descriptionMap[$el.data("index")].title
+    );
+    $(".class-section .description-tip").text(
+      descriptionMap[$el.data("index")].desc
+    );
+    $el.css(
+      "background-image",
+      `url('./bg-img/home-class-bg-${$el.data("index") + 1}.png')`
+    );
+  });
+  $(".class-section .class-wrap").mouseleave((event) => {
+    $(".class-section .class-wrap").css("background-image", "none");
+  });
 });
